5512383bdb190bf4aa3bfb2425cbab6cb6600331
brian-nguyen329 <brnguyen@ucsd.edu>
2021-02-18 10:42:04 -0800

    fin lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const e = require("express");
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -9,5 +11,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn() {
+	e.preventDefault();
+	ga('create', 'UA-190111028-1','auto');
+	ga('send','event','like','click');
 }
